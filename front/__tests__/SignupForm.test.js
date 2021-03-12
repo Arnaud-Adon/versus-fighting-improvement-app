9650beefa1af8bd859e06765e6c3f4999b98334b
Arnaud-Adon <assy.adon@yahoo.fr>
2021-03-13 00:03:42 +0100

    (Feat): Mise à jour du test SingnupForm

--- front/__tests__/SignupForm.test.js
+++ front/__tests__/SignupForm.test.js
@@ -1,19 +1,69 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react-native";
+import { Provider } from "react-redux";
+import { store } from "../lib/state/store";
+import {
+  render,
+  cleanup,
+  fireEvent,
+  waitFor,
+} from "@testing-library/react-native";
 import SignupForm from "../components/Sign/SignupForm";
-import { useCallback } from "react/cjs/react.development";
 
 describe("SignupForm Test", () => {
   afterEach(cleanup);
 
   it("should render SignupForm component", () => {
-    const SignupFormComponent = render(<SignupForm />);
+    const getWrapper = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    );
+    const SignupFormComponent = render(<SignupForm />, { wrapper: getWrapper });
 
     expect(SignupFormComponent).toBeTruthy();
   });
 
-  it("should return all register inputs needed", () => {
-    render(<SignupForm />);
+  it("should return all register input and submit button", () => {
+    const getWrapper = ({ children }) => (
+      <Provider store={store}>{children}</Provider>
+    );
+    const { getByTestId } = render(<SignupForm />, {
+      wrapper: getWrapper,
+    });
+
+    const pseudoInput = getByTestId("pseudo-input");
+    const emailInput = getByTestId("email-input");
+    const birthdayInput = getByTestId("birthday-input");
+    const countryInput = getByTestId("country-input");
+    const passwordInput = getByTestId("password-input");
+    const confirmPasswordInput = getByTestId("confirmPassword-input");
+    const submitButton = getByTestId("submit-button");
+
+    expect(pseudoInput).toBeTruthy();
+    expect(emailInput).toBeTruthy();
+    expect(birthdayInput).toBeTruthy();
+    expect(countryInput).toBeTruthy();
+    expect(passwordInput).toBeTruthy();
+    expect(confirmPasswordInput).toBeTruthy();
+    expect(submitButton).toBeTruthy();
+  });
+});
+
+it("should return a error when button is push and a input is empty", async () => {
+  const getWrapper = ({ children }) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  const { getAllByTestId, getByTestId } = render(<SignupForm />, {
+    wrapper: getWrapper,
+  });
+
+  //   const inputs = getAllByTestId(/input/);
+  const button = getByTestId("submit-button");
+
+  fireEvent.press(button);
+
+  await waitFor(() => {
+    const errors = getByTestId("error");
+    expect(errors.length).toBeGreaterThan(0);
   });
 });
 
